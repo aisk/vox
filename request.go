582769c68d6251b05f137bc2a0578cd4a251fda8
An Long <aisk1988@gmail.com>
2018-04-04 19:41:22 +0800

    :zap: embed http.Request in vox.Request for perfocemance

--- request.go
+++ request.go
@@ -7,23 +7,17 @@ import (
 
 // A Request stores current HTTP request's infomation.
 type Request struct {
-	Request *http.Request
-	Header  http.Header
-	Method  string
-	URL     *url.URL
-	Query   url.Values
-	Params  map[string]string
-	Origin  string
+	*http.Request
+	Query  url.Values
+	Params map[string]string
+	Origin string
 }
 
 func createRequest(raw *http.Request) *Request {
 	return &Request{
-		Request: raw,
-		Header:  raw.Header,
-		Method:  raw.Method,
-		URL:     raw.URL,
-		Query:   raw.URL.Query(),
-		Params:  map[string]string{},
-		Origin:  raw.Header.Get("Origin"),
+		raw,
+		raw.URL.Query(),
+		map[string]string{},
+		raw.Header.Get("Origin"),
 	}
 }
